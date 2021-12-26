0a7ec97b8027350ee662d3cd71b2ab3d638d2407
Ah Uzzol <uzzolah@gmail.com>
2021-12-26 21:40:26 +0600

    Admin redirect to dashboard after login

--- resources/views/admin/admin-index.blade.php
+++ resources/views/admin/admin-index.blade.php
@@ -86,8 +86,8 @@
                     </div>
 
                     <!-- Content Row -->
-                    <div class="row">rm -f .git/index.lock
-
+                    <div class="row">
+                        
                         <!-- Earnings (Monthly) Card Example -->
                         <div class="col-xl-3 col-md-6 mb-4">
                             <div class="card border-left-primary shadow h-100 py-2">
@@ -105,7 +105,7 @@
                                 </div>
                             </div>
                         </div>
-
+                        
                         <!-- Earnings (Monthly) Card Example -->
                         <div class="col-xl-3 col-md-6 mb-4">
                             <div class="card border-left-success shadow h-100 py-2">
